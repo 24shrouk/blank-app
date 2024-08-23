2fb4a2646752c847f09796c2cfe1e284b6e71235
24shrouk <143081315+24shrouk@users.noreply.github.com>
2024-08-23 18:47:38 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("🎈 Cars")
 st.number_input('feature 1',min_value=1,max_value=10,value=1)
+st.number_input('feature 2',min_value=1,max_value=10,value=1)
+st.number_input('feature 3',min_value=1,max_value=100,value=1)
