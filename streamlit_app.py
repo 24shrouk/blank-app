a5620fb90e69edd8e7ebaceb4e1f797abbe3c35b
24shrouk <143081315+24shrouk@users.noreply.github.com>
2024-08-23 18:54:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ st.title("🎈 Cars")
 f1=st.number_input('feature 1',min_value=1,max_value=10,value=1)
 f2=st.number_input('feature 2',min_value=1,max_value=10,value=1)
 f3=st.number_input('feature 3',min_value=1,max_value=100,value=1)
+
+
+with open('model.pkl','rb') as file:
+  model = pickle.load(file)
