a6d732dd17be7aa85f71a26e4ff4df03af3311b8
24shrouk <143081315+24shrouk@users.noreply.github.com>
2024-08-23 18:49:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pickle
 
 st.title("🎈 Cars")
-st.number_input('feature 1',min_value=1,max_value=10,value=1)
-st.number_input('feature 2',min_value=1,max_value=10,value=1)
-st.number_input('feature 3',min_value=1,max_value=100,value=1)
+f1=st.number_input('feature 1',min_value=1,max_value=10,value=1)
+f2=st.number_input('feature 2',min_value=1,max_value=10,value=1)
+f3=st.number_input('feature 3',min_value=1,max_value=100,value=1)
